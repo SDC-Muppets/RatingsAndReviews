f98f672231e9d8fc6283035f4174c55b4cb89a84
Clayton Chin <chinclayton98@gmail.com>
2022-08-05 15:13:55 -0700

    nothing added

--- __test__/k6tests/addReview.k6test.js
+++ __test__/k6tests/addReview.k6test.js
@@ -20,24 +20,7 @@ export default function () {
         "105": "4"
     }
   });
-  http.post('http://localhost:3000/reviews?product', {
-    "product_id": "111111",
-    "rating": "3",
-    "summary": "Twas an excellent piece of armor for my body.",
-    "body": "Tis armor doth glisten in the sun and blinds mine enemies.",
-    "recommend": "true",
-    "name": "SirDooby",
-    "email": "sirdooby@gmail.com",
-    "photos": ["https://www.darksword-armory.com/wp-content/uploads/2019/10/jousting-armor-1-scaled.jpg"],
-    "characteristics": {
-        "100": "1",
-        "101": "2",
-        "102": "3",
-        "103": "4",
-        "104": "5",
-        "105": "4"
-    }
-  });
+  http.post('http://localhost:3000/reviews?product', body);
   sleep(1);
 };
 
